1b9db02c3246621f07e53e33eb8b8972fbf5a5c1
Joseph Corcoran <josephcorcoran@Josephs-MacBook-Air.local>
2023-08-25 07:43:52 -0500

    update

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 import argparse
-from . import auth
+import auth
 
 
 if __name__ == "__main__":
@@ -7,9 +7,9 @@ if __name__ == "__main__":
                     description='What the program does')
     parser.add_argument('-ci', '--connected-app-id', default="3MVG9ux34Ig8G5eoX6TR_kdql8zB6vWl_AfSx9FXy2DOqqJIEnwugQHIjaDVgK48_0WKEpT0w_5eFeq9R.OAe", action='store', dest='connected_app_id')      # option that takes a value
     parser.add_argument('-sfdc-url', '--salesforce-url', default="https://test.salesforce.com", action='store', dest='salesforce_url')      # option that takes a value
-    parser.add_argument('-sfdc-user', '--sfdc-username', default="", action='store', dest='sfdc-username')      # option that takes a value
-    parser.add_argument('-key-file', '--key-file', default="", action='store', dest='keyfile_path')      # option that takes a value
+    parser.add_argument('-sfdc-user', '--sfdc-username', default="jbcorcoran85@gmail.com", action='store', dest='sfdc_username')      # option that takes a value
+    parser.add_argument('-key-file', '--key-file', default="jwt/server.key", action='store', dest='keyfile_path')      # option that takes a value
 
     env = parser.parse_args()
-    auth.simple_salesforce_auth(env.connected_app_id, env.salesforce_url, env.sfdc_username,env.keyfile_path)
+    auth.simple_salesforce_auth(env.sfdc_username, env.connected_app_id, env.salesforce_url, env.keyfile_path)
     auth.requests_auth()
--- auth.py
+++ auth.py
@@ -1,11 +1,8 @@
-import argparse
-#import click
-import requests
 from simple_salesforce import Salesforce
 
 
-def simple_salesforce_auth():
-    sf = Salesforce(username=env.sfdc_username, consumer_key=env.connected_app_id, privatekey_file='jwt/server.key', instance=env.salesforce_url)
+def simple_salesforce_auth(sfdc_username, connected_app_id, salesforce_url, privatekey_path):
+    sf = Salesforce(username=sfdc_username, consumer_key=connected_app_id, privatekey_file=privatekey_path, instance=salesforce_url)
     print(sf.headers['Authorization'])
 
 
@@ -16,13 +13,13 @@ def requests_auth():
     """
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-                    description='What the program does')
-    parser.add_argument('-ci', '--connected-app-id', default="3MVG9ux34Ig8G5eoX6TR_kdql8zB6vWl_AfSx9FXy2DOqqJIEnwugQHIjaDVgK48_0WKEpT0w_5eFeq9R.OAe", action='store', dest='connected_app_id')      # option that takes a value
-    parser.add_argument('-sfdc-url', '--salesforce-url', default="https://test.salesforce.com", action='store', dest='salesforce_url')      # option that takes a value
-    parser.add_argument('-sfdc-user', '--sfdc-username', default="", action='store', dest='sfdc-username')      # option that takes a value
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(
+#                     description='What the program does')
+#     parser.add_argument('-ci', '--connected-app-id', default="3MVG9ux34Ig8G5eoX6TR_kdql8zB6vWl_AfSx9FXy2DOqqJIEnwugQHIjaDVgK48_0WKEpT0w_5eFeq9R.OAe", action='store', dest='connected_app_id')      # option that takes a value
+#     parser.add_argument('-sfdc-url', '--salesforce-url', default="https://test.salesforce.com", action='store', dest='salesforce_url')      # option that takes a value
+#     parser.add_argument('-sfdc-user', '--sfdc-username', default="", action='store', dest='sfdc-username')      # option that takes a value
 
-    env = parser.parse_args()
-    simple_salesforce_auth()
-    requests_auth()
+#     env = parser.parse_args()
+#     simple_salesforce_auth()
+#     requests_auth()
